c3896ca3810330074ba1b7d56398f3337010f8cc
Liubou Levitskaya <lyubasaraeva@gmail.com>
2025-01-19 17:03:28 +0300

    feat: implement findLongestWord function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -332,8 +332,15 @@ function isPalindrome(/* str */) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const words = sentence.split(' ');
+  let longWord = '';
+  words.forEach(function findLongest(word) {
+    if (word.length > longWord.length) {
+      longWord = word;
+    }
+  });
+  return longWord;
 }
 
 /**
